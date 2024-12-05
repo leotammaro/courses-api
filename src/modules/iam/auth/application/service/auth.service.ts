9a528861359fffd9ada87411a926672c3126d493
Leonel Tammaro <leoneltammaro@MacBook-Pro-de-Leonel.local>
2024-12-05 10:09:22 -0300

    Add guard to validate if user already exists. Add method on user repository layer to get user by external id

--- src/modules/iam/auth/application/service/auth.service.ts
+++ src/modules/iam/auth/application/service/auth.service.ts
@@ -8,17 +8,27 @@ import {
   EXTERNAL_AUTH_SERVICE,
   IAuthExternalProvider,
 } from './firebase.service';
+import { UserService } from 'src/modules/iam/user/application/service/user.service';
+import { UserDto } from 'src/modules/iam/user/application/dto/user.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     @Inject(EXTERNAL_AUTH_SERVICE)
     private readonly authService: IAuthExternalProvider,
+    private readonly userService: UserService,
   ) {}
 
   async signUp(authDto: AuthDto) {
     try {
       const signUpResult = await this.authService.signUp(authDto);
+      const userData: UserDto = {
+        username: authDto.email,
+        externalId: signUpResult.externalId,
+        isVerified: false,
+      };
+      await this.userService.create(userData);
+
       return signUpResult;
     } catch (error) {
       console.error(error.message);
--- src/modules/iam/iam.module.ts
+++ src/modules/iam/iam.module.ts
@@ -7,16 +7,33 @@ import {
 } from './auth/application/service/firebase.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/entity/user.entity';
+import {
+  USER_REPOSITORY,
+  UserRepository,
+} from './user/infrastructure/database/user.repository';
+import { UserService } from './user/application/service/user.service';
+import { AuthGuard } from './auth/application/guard/auth.guard';
 
 const authServiceProvider: Provider = {
   provide: EXTERNAL_AUTH_SERVICE,
   useClass: FireBaseAuthService,
 };
 
+const userRepositoryProvider: Provider = {
+  provide: USER_REPOSITORY,
+  useClass: UserRepository,
+};
+
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
-  providers: [authServiceProvider, AuthService],
-  exports: [AuthService],
+  providers: [
+    authServiceProvider,
+    userRepositoryProvider,
+    AuthService,
+    UserService,
+    AuthGuard,
+  ],
+  exports: [AuthService, AuthGuard, UserService],
 })
 export class IamModule {}
